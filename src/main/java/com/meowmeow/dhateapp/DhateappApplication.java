2b3153a66b72c999a075714fb174889ebd409507
Jackmin801 <ongjackm@gmail.com>
2020-05-23 04:56:54 +0800

    Hello

---
+++ src/main/java/com/meowmeow/dhateapp/DhateappApplication.java
@@ -0,0 +1,13 @@
+package com.meowmeow.dhateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhateappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meowmeow/dhateapp/DhateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meowmeow.dhateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
