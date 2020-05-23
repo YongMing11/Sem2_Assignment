70893f94611aadf9722928cf8ba6940670a5c8a4
Jackmin801 <ongjackm@gmail.com>
2020-05-23 23:27:42 +0800

    Added gender lol

--- src/main/java/com/meowmeow/dhateapp/search/distanceSearchController.java
+++ src/main/java/com/meowmeow/dhateapp/search/distanceSearchController.java
@@ -1,4 +1,5 @@
 package com.meowmeow.dhateapp.search;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("search")
+@CrossOrigin("*")
 public class distanceSearchController {
     static List<searchResult> fake = List.of(
-            new searchResult("Chooi He Lin",100,5,"temp","temp"),
-            new searchResult("Choo She Lin",150,4,"temp","temp"),
-            new searchResult("Chooi He Lix",200,2,"temp","temp"),
-            new searchResult("Chooi Hee Ling",400,10,"temp","temp")
+            new searchResult("Chooi He Lin","Male",100,5,"temp","temp"),
+            new searchResult("Choo She Lin","Female",150,4,"temp","temp"),
+            new searchResult("Chu Helix","Male",200,2,"temp","temp"),
+            new searchResult("Chui Hi Ling","Prefer not to disclose",400,10,"temp","temp")
     );
     @GetMapping("/distance")
     public List<searchResult> placeholder(){
--- src/main/java/com/meowmeow/dhateapp/search/searchResult.java
+++ src/main/java/com/meowmeow/dhateapp/search/searchResult.java
@@ -2,17 +2,23 @@ package com.meowmeow.dhateapp.search;
 
 public class searchResult {
     private String username;
+    private String gender;
     private int distance;
     private int match;
     private String profileImageLink;
     private String profileLink;
 
-    public searchResult(String username, int distance, int match, String profileImageLink, String profileLink) {
+    public searchResult(String username, String gender, int distance, int match, String profileImageLink, String profileLink) {
         this.username = username;
+        this.gender = gender;
         this.distance = distance;
         this.match = match;
         this.profileImageLink = profileImageLink;
         this.profileLink = profileLink;
+    }
+
+    public String getGender() {
+        return gender;
     }
 
     public String getUsername() {
@@ -33,6 +39,10 @@ public class searchResult {
 
     public String getProfileLink() {
         return profileLink;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public void setUsername(String username) {
